51b1540a8f127345f7c877bc42dfaded681784d6
Crowdbotics <team@crowdbotics.com>
2021-06-07 15:53:36 +0000

    Update models

--- backend/soft_shape_27799/wsgi.py
+++ backend/soft_shape_27799/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'soft_shape_27799.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "soft_shape_27799.settings")
 
 application = get_wsgi_application()
